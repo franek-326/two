570acf71c289753149627dea295b637725c7af8e
franek-326 <mariusz.francuz@gmail.com>
2018-07-04 12:04:48 +0200

    lambda 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,17 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	System.out.print("Brawo Ty");// write your code here
+
+        System.out.print("Brawo Ty");// write your code here
+        ArrayList<String> beskidy;
+        beskidy = new ArrayList<String>();
+        beskidy.add("Beskid Wyspowy");
+        beskidy.add("Beskid Makowski");
+
+        beskidy.forEach((beskid) -> System.out.println(beskid));
     }
 }
